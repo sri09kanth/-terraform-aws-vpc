624892f7a67d4f20ef75098e7e8fcec1437397ec
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-03 12:54:45 +0530

    Update main.tf

--- examples/ipv6/main.tf
+++ examples/ipv6/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
   database_subnets = ["10.0.103.0/24", "10.0.104.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = fal
 
   create_database_subnet_route_table     = true
   create_database_internet_gateway_route = true
