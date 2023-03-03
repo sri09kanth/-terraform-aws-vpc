15a9c39188dad4a62b30eb210c432b6483744902
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-03 12:54:36 +0530

    Update main.tf

--- examples/ipv6/main.tf
+++ examples/ipv6/main.tf
@@ -22,7 +22,6 @@ module "vpc" {
 
   name = "ipv6"
   cidr = "10.0.0.0/16"
-
   azs              = ["${local.region}a", "${local.region}b"]
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
