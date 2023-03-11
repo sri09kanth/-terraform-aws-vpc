25cfb1156fcd16c8fbf45782f51747f6d359dac8
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-11 10:33:34 +0530

    Update outputs.tf

--- examples/simple-vpc/outputs.tf
+++ examples/simple-vpc/outputs.tf
@@ -14,7 +14,7 @@ output "vpc_cidr_block"
   value       = module.vpc.default_security_group_id
 }
 
-output "default_network_acl_id" {
+output "default_network_acl_id
   value       = module.vpc.default_network_acl_id
 }
 
