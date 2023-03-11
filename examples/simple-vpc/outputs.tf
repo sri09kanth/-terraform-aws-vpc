0a98564d759db37b593e91bcb6928a2cfa71ff4e
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-11 10:31:40 +0530

    Update outputs.tf

--- examples/simple-vpc/outputs.tf
+++ examples/simple-vpc/outputs.tf
@@ -12,7 +12,7 @@ output "vpc_cidr_block"
   description = "The CIDR block of the VPC"
   value       = module.vpc.vpc_cidr_blo
 
-output "default_security_group_id" {
+output "default_security_group_id"
   description = "The ID of the security group created by default on VPC creation"
   value       = module.vpc.default_security_group_id
 }
